dbfd5322edd576086898dbf6f336f571651fdbf2
Cat Smith <910384+catsmith@users.noreply.github.com>
2022-03-17 19:57:30 +0000

    adding required import

--- estoria_app/tasks.py
+++ estoria_app/tasks.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import subprocess
 import logging
 import os
