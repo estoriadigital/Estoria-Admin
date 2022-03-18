a2fd9227a4c0b0be207385d5d759666d20c68ab6
Cat Smith <910384+catsmith@users.noreply.github.com>
2022-03-18 21:41:45 +0000

    remove commented out code

--- xmlconversion_app/tasks.py
+++ xmlconversion_app/tasks.py
@@ -55,7 +55,6 @@ def xmlconversion(xml_filename, tempdir):
                     os.path.join(tempdir, 'edition/src/assets/scripts/'))
         shutil.copy(os.path.join(scripts_path, 'add_html_to_paginated_json.py'),
                     os.path.join(tempdir, 'edition/src/assets/scripts/'))
-        # print(os.path.join(tempdir, 'edition/scripts/'))
         logger.debug('{}: run make_paginated_json.py'.format(current_task.request.id))
         python_path = os.path.join(settings.VIRTUAL_ENV_PATH, 'bin', 'python3')
         subprocess.check_output([python_path, 'make_paginated_json.py', '-d', os.path.join(tempdir, 'edition/static/data')], cwd=os.path.join(tempdir, 'edition/src/assets/scripts/'), stderr=subprocess.STDOUT)
